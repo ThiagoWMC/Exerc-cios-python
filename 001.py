17ff0ba7904d475031a0cf4059aeea9171b9ba25
ThiagoWMC <thiagowmc@outlook.com>
2025-08-28 19:45:03 -0300

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,6 @@
+valor=float(input("Digite o valor da peça roupa:R$"))
+
+desconto =valor -  (valor * 5/100)
+
+print("O valor do desconto é de:R$",desconto)
+#Desenvolva um programa que calcule o valor do desconto de 5% 
---
+++ 002.py
@@ -0,0 +1,5 @@
+real = float(input("Digite quantos voce tem na carteira:R$ "))
+dolar = real / 5.25
+print("Com R${} voce pode comprar US${:.2f}".format(real, dolar))
+
+#Crie um programa que converta real para dolar
